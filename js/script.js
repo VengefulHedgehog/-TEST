81023cd9a8e3b5fbe34e81c6f9e4bb634b4bae95
KuzminykhEM <kuzja-280599@mail.ru>
2022-05-02 23:32:59 +0300

    +block case (PC)
    Добавление блока о кейсах для PC

--- js/script.js
+++ js/script.js
@@ -6,23 +6,40 @@ $(document).ready(function(){
 });
 
 
-
 $(function abc(){
     $('.client__content__slider').slick({
         prevArrow: '.client__content__arrow__prev',
         nextArrow: '.client__content__arrow__next',
         slidesToShow: 1,
-        responsive:[
+        responsive: [
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 4,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 970,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1300,
+                settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
                 }
             }
+
         ],
         mobileFirst:true,
     });
 });
+
+
 
 
 
